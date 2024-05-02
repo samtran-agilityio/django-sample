eef6db5a52924357312581263c49c89cfad75f71
sam.tran <sam.tran@asnet.com.vn>
2024-05-02 16:04:06 +0700

    -update allow host

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%k8394oj^le^b^yv@k!10vpy55%tkm6jglj0a$pwbb6x-6prnt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
